fe4cbfed4e097b8438ae919c6f15d7fefbed6bb2
Jannes Höke <mail@jhoeke.de>
2021-10-26 11:14:01 +0200

    ♻️ Pull city list from Google Sheet

--- scraper/src/load-cities.js
+++ scraper/src/load-cities.js
@@ -1,41 +1,17 @@
-const fs = require('fs');
 const got = require('got');
-const AdmZip = require('adm-zip');
-const { promisify } = require('util');
-const stream = require('stream');
-const temp = require('temp');
+const { csvParse } = require('d3-dsv');
 
 const { saveFile } = require('./io.js');
 
-const pipeline = promisify(stream.pipeline);
-// make sure temporary files get deleted
-temp.track();
-
-const url = 'http://download.geonames.org/export/dump/cities5000.zip';
+const url = process.env.CITIES_SHEETS_URL;
 
 module.exports = async () => {
-    return new Promise((resolve, reject) => temp.open('cities', async (err, info) => {
-        try {
-            await pipeline(got.stream(url), fs.createWriteStream(info.path));
-            // extract
-            const zip = new AdmZip(info.path);
-            const entry = zip.getEntries().find(e => e.entryName === 'cities5000.txt');
-            if (entry) {
-                const data = (await entry.getData().toString('utf8'))
-                    .split('\n')
-                    .map(line => line.split('\t'))
-                    .filter(row => row[8] === 'DE')
-                    .map(row => ({
-                        name: row[1],
-                        lat: +row[4],
-                        lon: +row[5]
-                    }));
-                await saveFile('cities.json', JSON.stringify(data));
-            }
-            resolve();
-        } catch (err) {
-            console.error('Could not read', url, 'due to', err);
-            reject(err);
-        }
-    }));
+	const response = await got(url);
+	const data = csvParse(response.body, (row) => {
+		delete row.duplicate;
+		row.lat = +row.lat;
+		row.lng = +row.lng;
+		return row;
+	});
+	await saveFile('cities.json', JSON.stringify(data));
 };
