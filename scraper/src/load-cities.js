3eeb4090a0a8decc71b753afc6e00969e141c11b
Jannes Höke <mail@jhoeke.de>
2021-10-26 12:04:56 +0200

    🎨 Fix formatting

--- scraper/src/load-cities.js
+++ scraper/src/load-cities.js
@@ -6,12 +6,12 @@ const { saveFile } = require('./io.js');
 const url = process.env.CITIES_SHEETS_URL;
 
 module.exports = async () => {
-	const response = await got(url);
-	const data = csvParse(response.body, (row) => {
-		delete row.duplicate;
-		row.lat = +row.lat;
-		row.lng = +row.lng;
-		return row;
-	});
-	await saveFile('cities.json', JSON.stringify(data));
+    const response = await got(url);
+    const data = csvParse(response.body, row => {
+        delete row.duplicate;
+        row.lat = +row.lat;
+        row.lng = +row.lng;
+        return row;
+    });
+    await saveFile('cities.json', JSON.stringify(data));
 };
